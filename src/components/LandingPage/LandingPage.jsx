dad37e8fcda6712fe1210dfd3a4bc7c8ea3f873e
fayfan <sbaltz98@gmail.com>
2025-04-14 16:50:11 -0500

    Finish landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,19 +3,21 @@ import './LandingPage.css';
 
 function LandingPage() {
   return (
-    <main>
-      <h1>Projects</h1>
-      <ul>
-        <li>
-          <NavLink to="/bonsai">Bonsai - Pricing Page</NavLink>
-        </li>
-        <li>
-          <NavLink to="/starbucks">Starbucks - Rewards Page</NavLink>
-        </li>
-        <li>
-          <NavLink to="/stark">Stark - Library Page</NavLink>
-        </li>
-      </ul>
+    <main id="landing-page">
+      <div>
+        <h1>Projects</h1>
+        <ul>
+          <li>
+            <NavLink to="/bonsai">Bonsai - Pricing Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/starbucks">Starbucks - Rewards Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/stark">Stark - Library Page</NavLink>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
